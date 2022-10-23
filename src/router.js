9e8cd73a8c523bf49d4e8d1b05c12e28772ba8c0
IsmailSalehCode <centriu78@gmail.com>
2022-10-23 16:19:55 +0300

    router

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "./views/HomePage.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  },
+});
+
+export default router;
