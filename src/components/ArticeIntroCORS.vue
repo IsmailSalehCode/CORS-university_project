c119298eb8e92e349773d2f979c56d1f021a2b5f
IsmailSalehCode <centriu78@gmail.com>
2022-10-24 00:26:24 +0300

    aye

--- src/components/ArticeIntroCORS.vue
+++ src/components/ArticeIntroCORS.vue
@@ -46,11 +46,12 @@
     </p>
     <p>
       Тъй като в сървъра на localhost:3000 е подходящо конфигуриран CORS (фиг.
-      {{ correctly_configured_cors_figure_num }}):
+      {{ correctly_configured_cors_figure_num }}), localhost:8080 има правото да
+      инвокира GET метод на route ‘/’ на порт 3000.
     </p>
     <figure>
       <img
-        style="max-width: 1000px"
+        style="max-width: 800px"
         src="../../media/properly-configured-cors.png"
         alt="Properly configured CORS"
       />
@@ -58,6 +59,31 @@
         Фиг. {{ correctly_configured_cors_figure_num }} Подходящо конфигуриран
         CORS на Node сървър
       </figcaption>
+    </figure>
+    <p>
+      Ако в Response Headers от фиг.
+      {{ cross_origin_request_successful_figure_num }} в
+      Access-Control-Allow-Origin полето имаше звезда (*) след двоеточието, то
+      това значи, че който и да е порт на localhost би могъл да достъпва този
+      ресурс, а не само 8080. За да постигнем това с нашия Node сървър, изграден
+      на Express фреймуърка, можем да подходим по два начина след като сме
+      import-нали cors модула от папката node_modules в константа, наименувана
+      cors (фиг. {{ correctly_configured_cors_figure_num }} – line 3 |
+      <code>const cors = require(“cors”);</code> ). Първият е с по-малко код
+      <code>app.use(cors();)</code>, а вторият с малко повече
+      <code>
+        const corsOptions = { origin: "*"}; app.use(cors(corsOptions));</code
+      >. И двата подхода гарантират всички routes на http://localhost:3000 да са
+      достъпни от който и да е порт на localhost (фиг.
+      {{ any_port_cors_figure_num }}).
+    </p>
+    <figure>
+      <img
+        style="max-width: 600px"
+        src="../../media/any-port-wildcard.png"
+        alt="Access-Control-Allow-Origin: Any port (*)"
+      />
+      <figcaption>Фиг. 3.5</figcaption>
     </figure>
   </article>
 </template>
@@ -68,6 +94,7 @@ export default {
     return {
       cross_origin_request_successful_figure_num: 1,
       correctly_configured_cors_figure_num: 3,
+      any_port_cors_figure_num: 3.5,
     };
   },
 };
