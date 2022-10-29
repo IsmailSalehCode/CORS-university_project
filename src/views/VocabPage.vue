a8e5cf245ad54b0c2112be6679638ff139997a37
IsmailSalehCode <centriu78@gmail.com>
2022-10-29 14:38:00 +0300

    yay

--- src/views/VocabPage.vue
+++ src/views/VocabPage.vue
@@ -15,35 +15,10 @@
     <hr />
     <br />
     <OriginArticle id="origin" />
-
     <br />
     <hr />
     <br />
-    <article id="html">
-      <h2>HTML</h2>
-      <p>
-        <b>HyperText Markup Language</b> (<b>HTML</b> накратко) е основният
-        маркиращ език за описание и дизайн на уеб страници. HTML е стандарт в
-        интернет, а неговите стандарти се определят от международния консорциум
-        W3C.
-      </p>
-      <p>
-        Описанието на документа става чрез специални елементи, наречени HTML
-        елементи или техните маркери, които се състоят от тагове и
-        съответстващите етикети (HTML tags) и ъглови скоби (като например
-        елемента <code>&lt;html&gt;</code>). HTML елементите са основната
-        градивна единица на кода, изграждащ уеб страниците. Чрез тях се
-        форматира, графично оформя текста и неговите отделните части в рамките
-        на една уеб страница, като например заглавия, цитати, текстови раздели,
-        хипертекстови препратки и т.н. Най-често HTML елементите са групирани по
-        двойки <code>&lt;h1&gt;</code> и <code>&lt;/h1&gt;</code>.
-      </p>
-      <p>
-        <a href="https://www.w3schools.com/html/default.asp" target="_blank"
-          >Научи повече тук...</a
-        >
-      </p>
-    </article>
+    <HtmlArticle id="html" />
     <br />
     <hr />
     <br />
@@ -57,6 +32,7 @@ import HttpHeaderArticle from "../components/HttpHeaderArticle.vue";
 import HttpStatusArticle from "../components/HttpStatusArticle.vue";
 import PortArticle from "../components/PortArticle.vue";
 import OriginArticle from "../components/OriginArticle.vue";
+import HtmlArticle from "../components/HtmlArticle.vue";
 
 export default {
   props: { word: String },
@@ -66,6 +42,7 @@ export default {
     DevToolsArticle,
     HttpHeaderArticle,
     HttpStatusArticle,
+    HtmlArticle,
   },
   mounted() {
     if (this.word != null) {
