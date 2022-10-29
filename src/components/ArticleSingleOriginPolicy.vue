9b912dc3b8f4da921f6ce1ccf1ac6b669930af73
IsmailSalehCode <centriu78@gmail.com>
2022-10-29 12:27:39 +0300

    aye

--- src/components/ArticleSingleOriginPolicy.vue
+++ src/components/ArticleSingleOriginPolicy.vue
@@ -6,52 +6,53 @@
       заявките, инициирани от скриптове. Tази политика се назовава
       <i>same-origin policy</i>. Ако направим заявка до сървър без CORS
       конфигурацията, следното съобщение излиза на конзолата на браузъра на 8080
-      (фиг. {{ default_sop_errors_figure_num }}):
+      (фиг. {{ default_sop_errors_figureNum }}):
     </p>
-    <figure>
-      <img
-        src="../../media/default-sop-errors.png"
-        alt="CORS errors from default Single Origin Policy"
-      />
-      <figcaption>
-        Фиг. {{ default_sop_errors_figure_num }} - CORS грешка; таргетният
-        сървър няма CORS headers => по подразбиране следва same-origin policy
-      </figcaption>
-    </figure>
+    <Figure
+      :figureNum="default_sop_errors_figureNum"
+      :imgData="default_sop_errors_figure"
+      figureCaption="CORS грешка; таргетният
+        сървър няма CORS headers => по подразбиране следва same-origin policy"
+      altCaption="CORS errors from default Single-Origin Policy"
+      maxWidthImg="900px"
+    />
     <p>
       В конзолата на Chrome Dev Tools се извеждат две error съобщения. Първото
       е, че направената заявка (XMLHttpRequest) от източник
-      http://localhost:8080 бе блокирана от CORS протокола, тъй като липсва
-      Access-Control-Allow-Origin заглавка в отговора на сървъра. Второто error
-      съобщение ни дава информация, че нашата GET заявка бе успешно получена (
-      => код 200 ОК), но отхвърлена от сървъра поради вече споменатите причини.
+      http://localhost:8080 бе блокирана от CORS протокола на 9gag, тъй като
+      липсва Access-Control-Allow-Origin заглавка в отговора на сървъра. Второто
+      error съобщение ни дава информация, че нашата GET заявка бе успешно
+      получена ( => HTTP код 200 ОК), но отхвърлена от сървъра поради вече
+      споменатите причини.
     </p>
     <p>
       Ръчно блокиране на външни скриптове да достъпват ресурсите на сървър с
       определен порт на localhost- в случая порт 3000 (фиг.
-      {{ manual_sop_config_figure_num }}):
+      {{ manual_sop_config_figureNum }}):
     </p>
-    <figure>
-      <img
-        style="max-width: 500px"
-        src="../../media/manual-sop-config.png"
-        alt="настройване сървъра на уеб галерията да следва same-origin policy, т.е само и единствено вътрешни скриптове да взаимодействат с route-овете на http://localhost:3000"
-      />
-      <figcaption>
-        Фиг. {{ manual_sop_config_figure_num }}- настройване сървъра на уеб
+    <Figure
+      :figureNum="manual_sop_config_figureNum"
+      :imgData="manual_sop_config_figure"
+      figureCaption="Настройване сървъра на уеб
         галерията да следва same-origin policy, т.е само и единствено вътрешни
-        скриптове да взаимодействат с route-овете на http://localhost:3000
-      </figcaption>
-    </figure>
+        скриптове да взаимодействат с route-овете на http://localhost:3000"
+      altCaption="Manual setting of Single Origin Policy"
+      maxWidthImg="500px"
+    />
   </article>
 </template>
 
 <script>
+import Figure from "./FigureDialog.vue";
+
 export default {
+  components: { Figure },
   data() {
     return {
-      default_sop_errors_figure_num: 4,
-      manual_sop_config_figure_num: 5,
+      default_sop_errors_figureNum: 4,
+      default_sop_errors_figure: require("../../media/default-sop-errors.png"),
+      manual_sop_config_figureNum: 5,
+      manual_sop_config_figure: require("../../media/manual-sop-config.png"),
     };
   },
 };
