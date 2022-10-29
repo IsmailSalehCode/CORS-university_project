f4c04660363ec2cb48fdae8f49e7ecc404248dba
IsmailSalehCode <centriu78@gmail.com>
2022-10-29 14:05:18 +0300

    ъеа

--- src/views/VocabPage.vue
+++ src/views/VocabPage.vue
@@ -25,11 +25,18 @@
       <h2>HTTP статус (Status)</h2>
       <p>
         Кодовете за състояние на HTTP отговор показват дали конкретна HTTP
-        заявка е изпълнена успешно. Отговорите са групирани в пет класа според <a href="https://httpwg.org/specs/rfc9110.html#overview.of.status.codes">RFC9110</a> стандарта:
-         <ol>
+        заявка е изпълнена успешно. Отговорите са групирани в пет класа според
+        <a href="https://httpwg.org/specs/rfc9110.html#overview.of.status.codes"
+          >RFC9110</a
+        >
+        стандарта:
+      </p>
+      <ol>
         <li>Информационни ( <code>100</code> - <code>199</code> )</li>
         <li>Успешни ( <code>200</code> - <code>299</code> )</li>
-        <li>Съобщения за пренасочване( <code>300</code> - <code>399</code> )</li>
+        <li>
+          Съобщения за пренасочване( <code>300</code> - <code>399</code> )
+        </li>
         <li>
           Клиентски отговори за грешки ( <code>400</code> - <code>499</code> )
         </li>
@@ -37,7 +44,6 @@
           Отговори за грешка на сървъра ( <code>500</code> - <code>599</code> )
         </li>
       </ol>
-      </p>
     </article>
     <br />
     <hr />
