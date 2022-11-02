cbebed43e89c915d831db33a126cd3bcd2368af2
IsmailSalehCode <centriu78@gmail.com>
2022-11-03 00:28:25 +0200

    separating footer in individual component

--- src/App.vue
+++ src/App.vue
@@ -51,48 +51,18 @@
       </v-list>
     </v-navigation-drawer>
     <!-- End of Mobile Menu -->
+
     <!-- Footer -->
-    <v-footer bottom absolute app dark>
-      <v-col cols="12">
-        <v-row style="text-align: center">
-          <v-col style="font-size: 0.9rem">
-            &#169; 2022 -
-            <strong>Исмаил Салех</strong>, студент в
-            <a href="https://www.tu-sofia.bg/" target="_blank"
-              >Технически университет - София</a
-            >
-          </v-col>
-        </v-row>
-      </v-col>
-      <v-col cols="12" style="padding-top: 0px">
-        <hr />
-      </v-col>
-      <v-col cols="12">
-        <v-row style="justify-content: center">
-          <ul id="ico">
-            <li
-              v-for="social in socialMediaItems"
-              :key="social.icon"
-              style="padding-right: 20px"
-            >
-              <a
-                :href="social.path"
-                target="_blank"
-                style="text-decoration: none"
-                ><v-icon>{{ social.icon }}</v-icon></a
-              >
-            </li>
-          </ul>
-        </v-row>
-      </v-col>
-    </v-footer>
+    <SiteFooter />
     <!-- End of Footer -->
   </v-app>
 </template>
 
 <script>
+import SiteFooter from "./components/SiteFooter.vue";
+
 export default {
-  name: "App",
+  components: { SiteFooter },
   data() {
     return {
       drawer: false,
@@ -114,24 +84,6 @@ export default {
           path: "/web-inspector",
         },
       ],
-      socialMediaItems: [
-        {
-          icon: "mdi-github",
-          path: "https://github.com/IsmailSalehCode",
-        },
-        {
-          icon: "mdi-facebook",
-          path: "https://www.facebook.com/ismail.saleh.921230",
-        },
-        {
-          icon: "mdi-instagram",
-          path: "https://www.instagram.com/ismails.2",
-        },
-        {
-          icon: "mdi-gmail",
-          path: "mailto:centriu78@gmail.com",
-        },
-      ],
     };
   },
 
@@ -144,10 +96,6 @@ export default {
 </script>
 
 <style>
-ul#ico {
-  display: inline-flex;
-  list-style-type: none;
-}
 #prTitle {
   color: white;
   border-top: 3px solid white;
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-footer bottom absolute app dark>
+    <v-col cols="12">
+      <v-row style="text-align: center">
+        <v-col style="font-size: 0.9rem">
+          &#169; 2022 -
+          <strong>Исмаил Салех</strong>, студент в
+          <a href="https://www.tu-sofia.bg/" target="_blank"
+            >Технически университет - София</a
+          >
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col cols="12" style="padding-top: 0px">
+      <hr />
+    </v-col>
+    <v-col cols="12">
+      <v-row style="justify-content: center">
+        <ul id="ico">
+          <li
+            v-for="social in socialMediaItems"
+            :key="social.icon"
+            style="padding-right: 20px"
+          >
+            <a :href="social.path" target="_blank" style="text-decoration: none"
+              ><v-icon>{{ social.icon }}</v-icon></a
+            >
+          </li>
+        </ul>
+      </v-row>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      socialMediaItems: [
+        {
+          icon: "mdi-github",
+          path: "https://github.com/IsmailSalehCode",
+        },
+        {
+          icon: "mdi-facebook",
+          path: "https://www.facebook.com/ismail.saleh.921230",
+        },
+        {
+          icon: "mdi-instagram",
+          path: "https://www.instagram.com/ismails.2",
+        },
+        {
+          icon: "mdi-gmail",
+          path: "mailto:centriu78@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul#ico {
+  display: inline-flex;
+  list-style-type: none;
+}
+</style>
