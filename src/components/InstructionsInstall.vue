8eea44a9dc267b9e9f7263daa440fc12adf43850
IsmailSalehCode <centriu78@gmail.com>
2022-11-02 21:27:06 +0200

    a

--- src/components/InstructionsInstall.vue
+++ src/components/InstructionsInstall.vue
@@ -3,10 +3,22 @@
     <h2>Инструкции за теглене на проекта</h2>
     <p>
     <ol class="vertical-list">
+        <li>
+            Изтеглете <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> за Вашия ОС.
+        </li>
       <li>
-        Изтеглете си
-        <a href="https://nodejs.org/en/download/" target="_blank">NodeJS</a>
+        Изтеглете
+        <a href="https://nodejs.org/en/download/" target="_blank">NodeJS</a> за Вашия ОС. npm трябва да е изтеглен глобално. Проверете като отворите конзолата си и въведете <span class='terminal-input'>npm -v</span>
       </li>
+      <li>Отворете с VSCode директорията, където бихте искали да се изтегли проекта.</li>
+      <li>От лявата вертикална лента идете в 'Source Control' и после натиснете бутона 'Initialize repository'. Ще се създаде локално repo.</li>
+      <li>Ако не ви се е отворила автоматично конзола във VSCode, изберете от лентата с инструменти Terminal -> New Terminal. Въведете в терминала командата:
+        <br>
+        <span class='terminal-input'>
+         git pull https://github.com/IsmailSalehCode/CORS-university_project.git
+        </span>
+        </li>
+      <li></li>
     </ol>
 
     </p>
@@ -20,5 +32,14 @@ export default {};
 <style>
 .vertical-list {
   line-height: 35px;
+}
+.terminal-input {
+  color: white;
+  background-color: black;
+  padding-right: 4px;
+  padding-left: 4px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  font-family: monospace;
 }
 </style>
